41be15a7e4320106eaa2a11e5d0ae20cd3743d36
silvajnr <silvajnr@gmail.com>
2019-04-19 16:41:36 +1000

    add migrations

---
+++ BlankApp1/BlankApp1/BlankApp1/DataStores/DesignTimeDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using BlankApp1.Services;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace BlankApp1.DataStores
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            Debug.WriteLine(Directory.GetCurrentDirectory() + @"\Config.db");
+            var url = new DataStoreDb();
+            return new ApplicationDbContext(url);
+        }
+    }
+
+    public class DataStoreDb : ISQLite
+    {
+        public string GetDbUrl()
+        {
+            return Directory.GetCurrentDirectory() + @"\Config.db";
+        }
+    }
+}
---
+++ BlankApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using BlankApp1.DataStores;
+using BlankApp1.Services;
+using Microsoft.EntityFrameworkCore.Design;
+using System.Diagnostics;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+        public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+        {
+            public ApplicationDbContext CreateDbContext(string[] args)
+            {
+                Debug.WriteLine(Directory.GetCurrentDirectory() + @"\Config.db");
+                var url = new DataStoreDb();
+                return new ApplicationDbContext(url);
+            }
+        }
+
+        public class DataStoreDb : ISQLite
+        {
+            public string GetDbUrl()
+            {
+                return Directory.GetCurrentDirectory() + @"\Config.db";
+            }
+        }
+    }
+}
